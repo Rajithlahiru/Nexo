0996e1c45b2ff58abe09461ed7553e75be8f2980
Rajithlahiru <rajithlahiru7@gmail.com>
2024-08-07 04:33:26 +0530

    eureka server / config server

---
+++ services/discovery/src/test/java/com/dea/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dea.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
