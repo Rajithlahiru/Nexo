ad7da429b5564045b4d652f7e7d60ab0f788ffc6
Bhanu Hasaranga <hasaranga20139@gmail.com>
2024-08-16 19:42:45 +0530

    implement order line controller

--- services/order/src/main/java/com/dea/ecommerce/orderline/OrderLineMapper.java
+++ services/order/src/main/java/com/dea/ecommerce/orderline/OrderLineMapper.java
@@ -17,4 +17,8 @@ public class OrderLineMapper {
                 .productID(request.productID())
                 .build();
     }
+
+    public OrderLineResponse toOrderLineResponse(OrderLine orderLine) {
+        return new OrderLineResponse(orderLine.getId(), orderLine.getQuantity());
+    }
 }
---
+++ services/order/src/main/java/com/dea/ecommerce/orderline/OrderLineResponse.java
@@ -0,0 +1,7 @@
+package com.dea.ecommerce.orderline;
+
+public record OrderLineResponse(
+        Integer id,
+        double quantity
+) {
+}
